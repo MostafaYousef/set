f38396698d861b5c7e28e8d95d97dfc7cb8bd958
MostafaYousef <mostafa.nonat@gmail.com>
2016-03-23 13:23:31 +0000

    initial project setup

---
+++ src/test/java/com/mostafa/set/SETApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mostafa.set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SETApplication.class)
+public class SETApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}
